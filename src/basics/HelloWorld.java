59d8a8ae2d69b0700194a7b67116064a99017058
Amedee <amedee@aquarus.net>
2023-02-04 11:59:15 +0100

    Made some small changes within the HelloWorlld app

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Amedee");
-		System.out.println("I am happy to learn Git and to share my Projects");
+		System.out.println("Currently I learn Java, HTML and CSS");
 	}
 }
