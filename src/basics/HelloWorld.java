1167b522f166148f35d40d531c469f0d445fc59d
Amedee <amedee@aquarus.net>
2023-02-04 12:03:12 +0100

    Deleted HelloWorld method

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,7 +3,6 @@ package basics;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Amedee");
 		System.out.println("Currently I learn Java, HTML and CSS");
 	}
 }
