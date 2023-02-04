5d6c101a290025b0fe9c6b88057aac31f4091279
Amedee <amedee@aquarus.net>
2023-02-04 12:05:38 +0100

    Made some small changes to the message within the HelloWorrdApp

--- src/basics/HelloWorld.java
+++ src/basics/HelloWorld.java
@@ -3,6 +3,6 @@ package basics;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Currently I learn Java, HTML and CSS");
+		System.out.println("Currently I learn Java, HTML and CSS and soon BootsTrap");
 	}
 }
